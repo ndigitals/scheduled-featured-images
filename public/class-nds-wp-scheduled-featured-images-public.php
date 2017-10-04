1c5f3d404ea1f9ec223be099d1973380de73536f
tim.nolte <tim.nolte@ndigitals.com>
2017-10-04 00:00:18 -0400

    * Fixes PHP formatting to pass beautifier tests.

--- public/class-nds-wp-scheduled-featured-images-public.php
+++ public/class-nds-wp-scheduled-featured-images-public.php
@@ -35,8 +35,8 @@ class Nds_Wp_Scheduled_Featured_Images_Public {
 	 * Initialize the class and set its properties.
 	 *
 	 * @since    1.0.0
-	 * @param      string    $plugin_name       The name of the plugin.
-	 * @param      string    $version    The version of this plugin.
+	 * @param      string $plugin_name       The name of the plugin.
+	 * @param      string $version    The version of this plugin.
 	 */
 	public function __construct( $plugin_name, $version ) {
 
